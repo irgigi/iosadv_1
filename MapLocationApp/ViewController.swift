8514ebda28b18a54e6a3a7b19ce4cb8bfcc08b85
irgigi <79409202+irgigi@users.noreply.github.com>
2023-12-05 13:22:58 +0500

    Initial Commit

---
+++ MapLocationApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MapLocationApp
+//
+//  Created by Мамуля on 05.12.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
