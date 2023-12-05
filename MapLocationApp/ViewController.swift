68e0aa33205c73c97bd4cd1527a28a7abf02b513
irgigi <79409202+irgigi@users.noreply.github.com>
2023-12-05 13:31:21 +0500

    init

--- MapLocationApp/ViewController.swift
+++ MapLocationApp/ViewController.swift
@@ -11,6 +11,7 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        view.backgroundColor = .lightGray
         // Do any additional setup after loading the view.
     }
 
